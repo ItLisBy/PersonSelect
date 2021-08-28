6710553515ae4a4486e5319a606e52bd977e7c25
LIS <l.ilya@outlook.com>
2021-08-28 10:52:24 +0300

    avaloniaui learning project

---
+++ src/PersonSelect/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using PersonSelect.Models;
+
+namespace PersonSelect.ViewModels
+{
+    public class TodoListViewModel : ViewModelBase
+    {
+        public TodoListViewModel(IEnumerable<TodoItem> items) {
+            Items = new ObservableCollection<TodoItem>(items);
+        }
+
+        public ObservableCollection<TodoItem> Items { get; }
+    }
+}
